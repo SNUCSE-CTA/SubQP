02315c026bd17fcffcbaafc3d8c011e6ad2e0579
Wonseok Shin <gratus907@snu.ac.kr>
2022-12-06 15:53:44 +0900

    developed subgraph query processing

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # SubgraphQueryProcessing
-Subgraph Query Processing
+Subgraph Query Processing. Handles multiple data graphs given as (name, path) pairs. 
+
+VEQ-S [1] is internally invoked to solve the problem. 
+
+## Environment
+This program requires VEQ executable file. 
+
+## Installation
+```sh
+git clone https://github.com/SNUCSE-CTA/SubgraphQueryProcessing
+```
+
+## Usage
+```sh
+python SubgraphQueryProcessing.py <VEQ binary path> <Graph list path> <Query graph path>
+```
+
+Graph list file contains name and path for multiple data graphs;
+```text
+name1 path1
+name2 path2
+...
+```
+
+Query graph path must be a path to GFU file. 
+
+## License
+Distributed under Apache License 2.0. See LICENSE for more information.
+
+## Reference
+[1] Hyunjoon Kim, Yunyoung Choi, Kunsoo Park, Xuemin Lin, Seok-Hee Hong, and Wook-Shin Han. 2021. Versatile Equivalences: Speeding up Subgraph Query Processing and Subgraph Matching. In Proceedings of the 2021 International Conference on Management of Data (SIGMOD '21). Association for Computing Machinery, New York, NY, USA, 925–937. https://doi.org/10.1145/3448016.3457265
---
+++ SubgraphQueryProcessing.py
@@ -0,0 +1,74 @@
+import os, sys
+from subprocess import Popen, PIPE
+
+
+def generate_args(binary, *params):
+    arguments = [binary]
+    arguments.extend(list(params))
+    return arguments
+
+
+def execute_binary(args):
+    cmd = ' '.join(args)
+    process = Popen(cmd, shell=True, stdout=PIPE, stderr=sys.stderr)
+    (std_output, std_error) = process.communicate()
+    process.wait()
+    rc = process.returncode
+    return rc, std_output, std_error
+
+
+def merge_gfu_graphs(graph_list):
+    file_list = list(map(
+        lambda x : x.strip().split(),
+        open(graph_list).readlines()
+    ))
+    merged_data = []
+    name_list = []
+    for i, (name, file_path) in enumerate(file_list):
+        name_list.append(name)
+        with open(file_path, 'r') as f:
+            _ = f.readline()
+
+            merged_data.append(f"#{i}")
+            num_vertices = int(f.readline().strip())
+            merged_data.append(str(num_vertices))
+
+            for i in range(num_vertices):
+                vertex_label = int(f.readline().strip())
+                merged_data.append(str(vertex_label))
+
+            merged_data.append(f.readline().strip())
+            num_edges = int(merged_data[-1])
+
+            for i in range(num_edges):
+                _inp = f.readline().strip()
+                merged_data.append(_inp)
+                edge = tuple(map(int, _inp.split()))
+            
+    return name_list, "\n".join(merged_data)
+
+if __name__ == "__main__":
+    binary_path = sys.argv[1]
+    graph_list = sys.argv[2]
+    query_graph = sys.argv[3]
+    name_list, merged_gfu = (merge_gfu_graphs(graph_list))
+
+    temp_data_path_ = "./tmp_input.gfu"
+    temporary_file_ = open(temp_data_path_, 'w')
+    temporary_file_.write(merged_gfu)
+    temporary_file_.close()
+
+    veq_output_path = "./search-result.txt"
+    execution_args = generate_args(binary_path, '-dg', temp_data_path_, '-qg', query_graph, '-o', veq_output_path)
+    (rc, std_output, std_error) = execute_binary(execution_args)
+    # std_output = str(std_output, encoding='utf-8')
+    # std_output_list = std_output.split('\n')
+    if rc != 0:
+        print("Error occured")
+        exit(1)
+
+    found_in = list(map(
+        lambda x : name_list[int(x.strip())],
+        open(veq_output_path).readlines()
+    ))
+    print(f"Found in :", *found_in)
